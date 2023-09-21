13a8fce0d9bbd8dac8020abfe749f5d2f9ba3bba
datengraben <datengraben@gmx.de>
2023-09-21 19:32:23 +0200

    add skripts to deploy and run

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+rsync -avh atom.xml root@shannes.de:/var/www/vhosts/datengraben.com/httpdocs/giessen-aktuelles.atom.xml
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/env bash
+jupyter nbconvert --execute --to python RSS-Posts-Gießen.ipynb && python3.10 RSS-Posts-Gießen.py
