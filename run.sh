5a4cf3a2c8bec71f183e57cf31f691009b86ce04
datengraben <datengraben@gmx.de>
2023-09-22 18:23:45 +0200

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-jupyter nbconvert --execute --to python RSS-Posts-Gießen.ipynb && python3.10 RSS-Posts-Gießen.py
-xmllint --format atom.xml > test.xml && mv test.xml atom.xml
+# jupyter nbconvert --execute --to python RSS-Posts-Gießen.ipynb &&
+python3.10 RSS-Posts-Gießen.py -s && xmllint --format atom.xml > test.xml && mv test.xml atom.xml
